624cdcc864fcc4f3f6276858680f722b2cbdb7f1
A Tawfique <easternselling@gmail.com>
2020-01-31 15:55:09 -0500

    Git added

--- bank.rb
+++ bank.rb
@@ -1,8 +1,9 @@
 class Bank
-
   def initialize
     @customer_data = []
     @account_data = []
+    customer_filler
+    account_filler
   end
 
   def customer_filler
@@ -25,27 +26,63 @@ class Bank
 end
 
 class Customer < Bank
-  attr_accessor :customer_number, :name, :social, :address
-  def initialize(customer_number, name, social, address)
-    @customer_number = customer_number
-    @name = name
-    @social = social
-    @address = address
-    @cust_info = []
+  attr_accessor :customer_number, :first, :last, :social, :address
+
+  # def initialize
+  #   @customer_number
+  #   @first
+  #   @last
+  #   @social
+  #   @address
+  # end
+  @customer_data
+  def create_customer(arr)
+    @customer_number = arr[0]
+    @first = arr[1]
+    @last = arr[2]
+    @social = arr[3]
+    @address = arr[4]
+
+    file = File.open(File.dirname(__FILE__) + "/data/customer.txt", "a") do |line|
+      arr.each_with_index do |thing, index|
+        if index === 4
+          line.write "#{thing}".chomp
+        elsif index === 0
+          line.write "#{thing}_"
+        else
+          line.write "#{thing}_"
+        end
+      end
+      line.puts ""
+    end
   end
-  def cust_info
-    puts "#{@customer_number}, #{@name}, #{@social}, #{@address}"
-  end
-  def push_to_db
-    @cust_info << [@customer_number, @name, @social, @address]
+  #
+  def login_customer_data(customer_number)
+    @customer_data.each_with_index do |line, index|
+      #p line[0]
+      if line[0] === customer_number.to_s
+        @customer_number = line[0]
+        @first = line[1]
+        @last = line[2]
+        @social = line[3]
+        @address = line[4]
+      end
+    end
   end
 end
 
 class Account < Customer
   attr_accessor :account, :account_type, :balance, :last_accessed
   @account_array = []
-  def create_array
-    @account_array << [@customer_number, @account, @account_type, @balance, @last_accessed]
+  def set_account_data(customer_number, account_type)
+    @account_data.each_with_index do |line, index|
+      if line[0] === customer_number.to_s and line[2] === account_type
+        @account = line[1]
+        @account_type = line[2]
+        @balance = line[3]
+        @last_accessed = line[4]
+      end
+    end
   end
   #p @account_array
 end
@@ -58,12 +95,33 @@ class Transaction < Account
 
   end
 
-  def deposit(to_account, how_much)
-
+  def deposit(how_much)
+    puts "Which account would you like to deposit to?"
+    puts "(C)hecking"
+    puts "(S)avings"
+    account_type = gets.chomp.to_s
+    if account_type === 'c'
+      type = 'checking'
+    elsif type === 's'
+      type = 'savings'
+    end
+    set_account_data(@customer_number, type)
+    @balance = (@balance.to_i) + (how_much.to_i)
   end
 
-  def withdraw(from_account, how_much)
-
+  def withdrawl(how_much)
+    puts "Which account would you like to withdrawl from?"
+    puts "(C)hecking"
+    puts "(S)avings"
+    account_type = gets.chomp.to_s
+    if account_type === 'c'
+      type = 'checking'
+    elsif type === 's'
+      type = 'savings'
+    end
+    set_account_data(@customer_number, type)
+    @balance = (@balance.to_i) - (how_much.to_i)
+    puts @balance
   end
 
   def transfer(from_account, to_account, how_much)
@@ -75,66 +133,68 @@ class Transaction < Account
 
   def interact
     #call banks get info from file functions here
-    get_customer_data
-    get_account_data
-
+    # @customer_data
     puts "Welcome to the bank, how may we help you?"
     puts "(c)reate new account"
     puts "(l)ogin with customer number"
     puts "(e)xit"
     r1 = gets.chomp.to_s
     if r1.downcase === 'c'
-      #create account here and then loop back around to interact again
+      puts "Enter your first name"
+      f_name = gets.chomp.to_s
+      puts "Enter your last name"
+      l_name = gets.chomp.to_s
+      puts "Enter your SSN"
+      ssn = gets.chomp.to_s
+      puts "Enter your Address"
+      address = gets.chomp.to_s
+
+      len = @customer_data.length
+
+      arr = [((len + 1).to_s), f_name, l_name, ssn, address]
+
+      @customer_data.append(arr)
+      create_customer(arr)
+
+
     elsif r1.downcase === 'l'
       puts "Welcome back, enter customer number:"
       temp_c_num = gets.chomp.to_s
+      login_customer_data(temp_c_num)
       #do a check to see if temp_c_num exists in @customer_data if so then
       # parse customer_data and fill in @customer_number & @name & @social &@address such as @customer_number = whatever
       #also parse @account_data and fill in the appropriate account information
       #if successfully logged in
-      puts "Welcome #{@name}!, what would you like to do today?"
+      puts "Welcome #{@first}!, what would you like to do today?"
+      puts "See account (b)alances"
       puts "Make a (w)ithdrawal"
       puts "Make a (d)eposit"
       puts "Make a (t)ransfer"
       r2 = gets.chomp.to_s
       if r2.downcase === 'w'
-        if @account_type === 'cs'
-          puts "Okay, which account do you want to withdraw from?"
-          puts "(c)hecking"
-          puts "(s)avings"
-          which_account = gets.chomp.to_s
-          if which_account === 'c'
-            puts "Great, withdraw how much?"
-            how_much = gets.chomp.to_s
-            withdraw(@checking_number, how_much)
-            interact
-          elsif which_account === 's'
-            puts "Great, withdraw how much?"
-            how_much = gets.chomp.to_s
-            withdraw(@savings_number, how_much)
-            interact
-          end
-        elsif @account_type === 'c'
-          puts "Great, withdraw how much?"
+          puts "Great, withdrawl how much?"
           how_much = gets.chomp.to_s
-          withdraw(@checking_number, how_much)
-          interact
-        elsif @account_type === 's'
-          puts "Great, withdraw how much?"
-          how_much = gets.chomp.to_s
-          withdraw(@savings_number, how_much)
-          interact
+          withdrawl(how_much)
+      elsif r2.downcase === 'b'
+        puts "(C)hecking"
+        puts "(S)avings"
+        type = gets.chomp.to_s
+
+        if type === 'c'
+          type = 'checking'
+        elsif type === 's'
+          type = 'savings'
         end
-        #if unsucessfully logged in
-        puts "customer number does not exist"
-        #interact
-      elsif r1.downcase === 'd'
+
+        set_account_data(temp_c_num, type)
+
+        puts "Your #{type} balance is $#{@balance}"
+      elsif r2.downcase === 'd'
         #IF THEY HAVE BOTH CHECKING AND SAVINGS THEN ASK WHICH ONE THEY WANT TO WITHDRAW FROM
         # IF THEY ONLY HAVE ONE OR THE OTHER JUST JUMP TO
         puts "Great, deposit how much?"
         how_much = gets.chomp.to_s
-        deposit(to_where, how_much)
-        interact
+        deposit(how_much)
       elsif r1.downcase === 't'
         puts "Transfer from?"
         # only puts this is they have a checking acc
@@ -187,3 +247,6 @@ end
 
 
 #Bank
+#
+i = Transaction.new()
+i.interact
