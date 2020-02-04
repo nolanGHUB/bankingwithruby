2e968abc32896d19e1a7c3f625ee7de11b1b8970
A Tawfique <easternselling@gmail.com>
2020-02-04 13:11:14 -0500

    Push

--- bank.rb
+++ bank.rb
@@ -1,3 +1,7 @@
+require 'hirb'
+require 'pp'
+require 'colorize'
+
 class Bank
   def initialize
     @customer_data = []
@@ -44,6 +48,7 @@ class Customer < Bank
     @address = arr[4]
 
     file = File.open(File.dirname(__FILE__) + "/data/customer.txt", "a") do |line|
+      line.puts ""
       arr.each_with_index do |thing, index|
         if index === 4
           line.write "#{thing}".chomp
@@ -55,6 +60,8 @@ class Customer < Bank
       end
       line.puts ""
     end
+
+    puts "Customer Created!".green
   end
   #
   def login_customer_data(customer_number)
@@ -96,9 +103,9 @@ class Transaction < Account
   end
 
   def deposit(how_much)
-    puts "Which account would you like to deposit to?"
-    puts "(C)hecking"
-    puts "(S)avings"
+    puts "Which account would you like to deposit to?".blue
+    puts "(C)hecking".green
+    puts "(S)avings".green
     account_type = gets.chomp.to_s
     if account_type === 'c'
       type = 'checking'
@@ -110,9 +117,9 @@ class Transaction < Account
   end
 
   def withdrawl(how_much)
-    puts "Which account would you like to withdrawl from?"
-    puts "(C)hecking"
-    puts "(S)avings"
+    puts "Which account would you like to withdrawl from?".blue
+    puts "(C)hecking".green
+    puts "(S)avings".green
     account_type = gets.chomp.to_s
     if account_type === 'c'
       type = 'checking'
@@ -134,19 +141,21 @@ class Transaction < Account
   def interact
     #call banks get info from file functions here
     # @customer_data
-    puts "Welcome to the bank, how may we help you?"
-    puts "(c)reate new account"
-    puts "(l)ogin with customer number"
-    puts "(e)xit"
+    puts "Welcome to the bank, how may we help you?".blue
+    puts "(c)reate new account".green
+    puts "(l)ogin with customer number".green
+    puts "(s)how all accounts".green
+    puts "(r)ecords of transactions".green
+    puts "(e)xit".red
     r1 = gets.chomp.to_s
     if r1.downcase === 'c'
-      puts "Enter your first name"
+      puts "Enter your first name".green
       f_name = gets.chomp.to_s
-      puts "Enter your last name"
+      puts "Enter your last name".green
       l_name = gets.chomp.to_s
-      puts "Enter your SSN"
+      puts "Enter your SSN".green
       ssn = gets.chomp.to_s
-      puts "Enter your Address"
+      puts "Enter your Address".green
       address = gets.chomp.to_s
 
       len = @customer_data.length
@@ -155,29 +164,30 @@ class Transaction < Account
 
       @customer_data.append(arr)
       create_customer(arr)
+      puts "Account created!".yellow
 
 
     elsif r1.downcase === 'l'
-      puts "Welcome back, enter customer number:"
+      puts "Welcome back, enter customer number:".green
       temp_c_num = gets.chomp.to_s
       login_customer_data(temp_c_num)
       #do a check to see if temp_c_num exists in @customer_data if so then
       # parse customer_data and fill in @customer_number & @name & @social &@address such as @customer_number = whatever
       #also parse @account_data and fill in the appropriate account information
       #if successfully logged in
-      puts "Welcome #{@first}!, what would you like to do today?"
-      puts "See account (b)alances"
-      puts "Make a (w)ithdrawal"
-      puts "Make a (d)eposit"
-      puts "Make a (t)ransfer"
+      puts "Welcome #{@first}!, what would you like to do today?".blue
+      puts "See account (b)alances".green
+      puts "Make a (w)ithdrawal".green
+      puts "Make a (d)eposit".green
+      puts "Make a (t)ransfer".green
       r2 = gets.chomp.to_s
       if r2.downcase === 'w'
-          puts "Great, withdrawl how much?"
+          puts "Great, withdrawl how much?".blue
           how_much = gets.chomp.to_s
           withdrawl(how_much)
       elsif r2.downcase === 'b'
-        puts "(C)hecking"
-        puts "(S)avings"
+        puts "(C)hecking".green
+        puts "(S)avings".green
         type = gets.chomp.to_s
 
         if type === 'c'
@@ -188,19 +198,19 @@ class Transaction < Account
 
         set_account_data(temp_c_num, type)
 
-        puts "Your #{type} balance is $#{@balance}"
+        puts "Your #{type} balance is $#{@balance}".blue
       elsif r2.downcase === 'd'
         #IF THEY HAVE BOTH CHECKING AND SAVINGS THEN ASK WHICH ONE THEY WANT TO WITHDRAW FROM
         # IF THEY ONLY HAVE ONE OR THE OTHER JUST JUMP TO
-        puts "Great, deposit how much?"
+        puts "Great, deposit how much?".blue
         how_much = gets.chomp.to_s
         deposit(how_much)
       elsif r1.downcase === 't'
-        puts "Transfer from?"
+        puts "Transfer from?".blue
         # only puts this is they have a checking acc
-        puts "(C)hecking"
+        puts "(C)hecking".green
         # only puts this is they have a savings acc
-        puts "(S)aving"
+        puts "(S)aving".green
         r3 = gets.chomp.to_s
         if r3.downcase === 'c'
           # functionality to transfer from checking
@@ -228,7 +238,46 @@ class Transaction < Account
     elsif r1 === 'e'
       puts "Exiting!"
       #run some sort of cleanup and re-write all 3 files with new info
+    elsif r1.downcase === 's'
+
+      data = []
+
+      file = File.open(File.dirname(__FILE__) + '/data/customer.txt', "r") do |line|
+
+        line.each do |record|
+          # puts record
+          r = record.chomp
+          data.append(r.split("_"))
+        end
+      end
+
+      puts Hirb::Helpers::AutoTable.render(
+          data,
+          fields: [0, 1, 2, 3, 4],  #Specify the index positions in each row array to include in the table and their column order in the table
+          headers: {0 => 'Account #', 1 => 'First Name', 2 => "Last Name", 3 => "SSN", 4 => "Address"},  #Convert the column headers to something more desirable
+          description: false #Get rid of "3 rows in set" following the table
+      )
+    elsif r1.downcase === 'r'
+      data = []
+
+      file = File.open(File.dirname(__FILE__) + '/data/transaction.txt', "r") do |line|
+
+        line.each do |record|
+          # puts record
+          r = record.chomp
+          data.append(r.split("_"))
+        end
+      end
+
+      puts Hirb::Helpers::AutoTable.render(
+          data,
+          fields: [0, 1, 2, 3, 4],  #Specify the index positions in each row array to include in the table and their column order in the table
+          headers: {0 => 'Account #', 1 => 'Transaction #', 2 => "Type", 3 => "Amount", 4 => "Date/Time"},  #Convert the column headers to something more desirable
+          description: false #Get rid of "3 rows in set" following the table
+      )
     end
+
+
   end
 end
 
--- file-test.rb
+++ file-test.rb
@@ -5,7 +5,7 @@ require 'colorize'
 arr = [9004, "John", "Doe", "103-96-3912", "192 Main St, Manhattan, NY 10001"]
 data = []
 
-file = File.open(File.dirname(__FILE__) + "/test", "a") do |line|
+file = File.open(File.dirname(__FILE__) + "/data/customer.txt", "a") do |line|
   arr.each_with_index do |thing, index|
     if index === 4
       line.write "#{thing}".chomp
@@ -18,16 +18,18 @@ file = File.open(File.dirname(__FILE__) + "/test", "a") do |line|
   line.puts ""
 end
 
-file = File.open(File.dirname(__FILE__) + '/test', "r") do |line|
+file = File.open(File.dirname(__FILE__) + '/data/customer.txt', "r") do |line|
   line.each do |record|
     # puts record
-    data.append(record.split("_"))
+    r = record.chomp
+    data.append(r.split("_"))
   end
 end
 
 puts "I am now red".red
 puts "I am now blue".blue
 puts "Testing".yellow
+
 puts Hirb::Helpers::AutoTable.render(
     data,
     fields: [0, 1, 2, 3, 4],  #Specify the index positions in each row array to include in the table and their column order in the table
